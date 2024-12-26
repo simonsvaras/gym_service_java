bf28bb1506fc9540cc462a005f962dadcae9b39b
simonTerminator <simonsvara@gmail.com>
2024-12-26 21:39:58 +0100

    Entity User

--- src/main/java/com/gym/gymmanagementsystem/entities/User.java
+++ src/main/java/com/gym/gymmanagementsystem/entities/User.java
@@ -1,4 +1,66 @@
 package com.gym.gymmanagementsystem.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userID;
+
+    @Column(length = 50, nullable = false)
+    private String firstname;
+
+    @Column(length = 50, nullable = false)
+    private String lastname;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 255)
+    private String password;
+
+    private LocalDate birthdate;
+
+    @Column(length = 200)
+    private String profilePhoto;
+
+    // Jedna karta na uživatele
+    @OneToOne
+    @JoinColumn(name = "ID_Card", unique = true)
+    private Card card;
+
+    // Aktivní subscription
+    @OneToOne
+    @JoinColumn(name = "ID_Subscription", unique = true)
+    private UserSubscription activeSubscription;
+
+    private Boolean realUser = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // Vztahy
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserSubscription> userSubscriptions;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserOneTimeEntry> userOneTimeEntries;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TransactionHistory> transactionHistories;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EntryHistory> entryHistories;
 }
