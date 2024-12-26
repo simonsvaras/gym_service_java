748535df5da887ea0a20769ca721d30716fa6d53
simonTerminator <simonsvara@gmail.com>
2024-12-26 20:59:31 +0100

    Entity Employee

--- src/main/java/com/gym/gymmanagementsystem/entities/Employee.java
+++ src/main/java/com/gym/gymmanagementsystem/entities/Employee.java
@@ -1,4 +1,37 @@
 package com.gym.gymmanagementsystem.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEmployee;
+
+    @Column(length = 100, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String firstname;
+
+    @Column(length = 100, nullable = false)
+    private String lastname;
+
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    @Column(length = 15)
+    private String role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
