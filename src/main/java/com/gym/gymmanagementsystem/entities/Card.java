eae3d9513f4f5d3383094957d031ea109e28e49e
simonTerminator <simonsvara@gmail.com>
2024-12-26 21:04:13 +0100

    Entity Card

--- src/main/java/com/gym/gymmanagementsystem/entities/Card.java
+++ src/main/java/com/gym/gymmanagementsystem/entities/Card.java
@@ -1,4 +1,31 @@
 package com.gym.gymmanagementsystem.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Cards")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cardID;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String cardNumber;
+
+    private Boolean lost;
+
+    @Column(length = 50)
+    private String cardType;
+
+    // Vztah s uživatelem (jedna karta může patřit jednomu uživateli)
+    @OneToOne(mappedBy = "card")
+    private User user;
 }
