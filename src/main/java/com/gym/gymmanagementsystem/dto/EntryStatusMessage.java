8b370e229d19c55fa879f70f329e8ac26680cb4d
simonsvaras <58275522+simonsvaras@users.noreply.github.com>
2025-07-08 10:56:11 +0200

    Add DTO for WebSocket entry status messages

---
+++ src/main/java/com/gym/gymmanagementsystem/dto/EntryStatusMessage.java
@@ -0,0 +1,22 @@
+package com.gym.gymmanagementsystem.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Message sent over WebSocket containing information about the result of an entry validation.
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EntryStatusMessage {
+
+    private String userId;
+
+    private String status;
+
+    private Integer remainingEntries;
+
+    private LocalDate expiryDate;
+}
